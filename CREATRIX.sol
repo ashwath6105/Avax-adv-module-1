348fc6096131d34ee3af7900dcced5da8b88b10b
ashwath6105 <141135626+ashwath6105@users.noreply.github.com>
2023-09-16 19:39:40 +0530

    Update CREATRIX.sol

--- CREATRIX.sol
+++ CREATRIX.sol
@@ -1,1 +1,169 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract ERC20 {
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+    string public name = "CREATRIX";
+    string public symbol = "CTX";
+    uint8 public decimals = 18;
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+
+    uint public playerLevel;
+    mapping(address => uint[3]) public userFoundItems; // Store found items per user [0: Sword, 1: Shield, 2: BattleAxe]
+    string public IG = "In-game items are: 1. Sword 2. Shield 3. Battle Axe";
+    string public itemFound; // Store the name of the last item found
+
+    constructor() {
+        playerLevel = 1;
+    }
+
+    enum Item { Sword, Shield, BattleAxe }
+
+    function transfer(address recipient, uint amount) external returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function mint(uint amount) external {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function burn(uint amount) external {
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+    }
+
+    function findItem(uint itemNumber) external returns (string memory) {
+        require(itemNumber >= 0 && itemNumber <= 2, "Invalid item number");
+
+        Item item = Item(itemNumber);
+
+        if (userFoundItems[msg.sender][itemNumber] < 1) {
+            if (item == Item.Sword) {
+                playerLevel += 1;
+                userFoundItems[msg.sender][itemNumber] += 1;
+                itemFound = "Sword";
+            } else if (item == Item.Shield) {
+                playerLevel += 2;
+                userFoundItems[msg.sender][itemNumber] += 1;
+                itemFound = "Shield";
+            } else if (item == Item.BattleAxe) {
+                playerLevel += 3;
+                userFoundItems[msg.sender][itemNumber] += 1;
+                itemFound = "BattleAxe";
+            }
+            return itemFound;
+        }
+
+        revert("You have already found this item");
+    }
+}
+
+
+interface IERC20 {
+    function totalSupply() external view returns (uint);
+
+    function balanceOf(address account) external view returns (uint);
+
+    function transfer(address recipient, uint amount) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool);
+
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+}
+
+contract Vault {
+    IERC20 public immutable token;
+
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+
+    function _mint(address _to, uint _shares) private {
+        totalSupply += _shares;
+        balanceOf[_to] += _shares;
+    }
+
+    function _burn(address _from, uint _shares) private {
+        totalSupply -= _shares;
+        balanceOf[_from] -= _shares;
+    }
+
+    function deposit(uint _amount) external {
+        /*
+        a = amount
+        B = balance of token before deposit
+        T = total supply
+        s = shares to mint
+
+        (T + s) / T = (a + B) / B 
+
+        s = aT / B
+        */
+        uint shares;
+        if (totalSupply == 0) {
+            shares = _amount;
+        } else {
+            shares = (_amount * totalSupply) / token.balanceOf(address(this));
+        }
+
+        _mint(msg.sender, shares);
+        token.transferFrom(msg.sender, address(this), _amount);
+    }
+
+    function withdraw(uint _shares) external {
+        /*
+        a = amount
+        B = balance of token before withdraw
+        T = total supply
+        s = shares to burn
+
+        (T - s) / T = (B - a) / B 
+
+        a = sB / T
+        */
+        uint amount = (_shares * token.balanceOf(address(this))) / totalSupply;
+        _burn(msg.sender, _shares);
+        token.transfer(msg.sender, amount);
+    }
+}
 
